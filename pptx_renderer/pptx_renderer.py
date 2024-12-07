bd3c686d136c2bcd78e32550c735c5da837dc1f9
Najeem Muhammed <idling-mind@users.noreply.github.com>
2024-12-07 22:06:19 +0100

    Loop groups (#15)
    * Loop groups
    
    * Added tests for loop_groups

--- pptx_renderer/pptx_renderer.py
+++ pptx_renderer/pptx_renderer.py
@@ -3,7 +3,7 @@
 import re
 from os import PathLike
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, Callable
+from typing import Any, Dict, Optional, Union, Callable, List
 from warnings import warn as warning
 from functools import partial
 from . import plugins
@@ -12,7 +12,7 @@ from pptx import Presentation
 from pptx.enum.shapes import MSO_SHAPE_TYPE
 
 from .exceptions import RenderError
-from .utils import fix_quotes, para_text_replace
+from .utils import fix_quotes, para_text_replace, copy_slide, clear_presentation
 
 PLUGINS = [plugins.image, plugins.video, plugins.table]
 
@@ -60,6 +60,7 @@ class PPTXRenderer:
         output_path: Union[str, PathLike],
         methods_and_params: Optional[Dict[str, Any]] = None,
         skip_failed: bool = False,
+        loop_groups: Optional[List[Dict[str, Any]]] = None,
     ) -> None:
         """Render PPTXRenderer template and save to output_path.
 
@@ -69,14 +70,26 @@ class PPTXRenderer:
                 to be used in the template. Defaults to None.
             skip_failed (bool, optional): Dont raise an error if some of the
                 statements failed to render. Defaults to False.
+            loop_groups (list, optional): List of dictionaries containing the
+                following keys:
+                - start: Slide number where the loop starts
+                - end: Slide number where the loop ends
+                - variable: Variable name to be used in the loop
+                - iterable: Iterable to loop over
+                Defaults to None.
 
         Returns:
             None
         """
+        self.template_path = str(self.template_path)
         if not Path(self.template_path).exists():
             raise (FileNotFoundError(f"{self.template_path} not found"))
-        outppt = Presentation(self.template_path)
+        template_ppt = Presentation(self.template_path)
+        if not methods_and_params:
+            methods_and_params = {}
         self.namespace.update(methods_and_params)
+        if not loop_groups:
+            loop_groups = []
 
         #function to recurse through list of shapes
         #hand off to function to process text frames and tables
@@ -105,13 +118,13 @@ class PPTXRenderer:
                         )
                         return
                     raise RenderError(
-                        f"Failed to evaluate '{parts[0]}'."
+                        f"Failed to evaluate '{parts[0]}' in slide {slide_no+1}."
                     ) from ex
                 if len(parts) > 1:
                     namespace = self.namespace.copy()
                     context = {
                         "result": result,
-                        "presentation": outppt,
+                        "presentation": template_ppt,
                         "shape": shape,
                         "slide": slide,
                         "slide_no": slide_no,
@@ -160,8 +173,37 @@ class PPTXRenderer:
                                 paragraph, match_assignment.group(0), result
                             )
 
-        for slide_no, slide in enumerate(outppt.slides):
-            if slide.has_notes_slide:
+        output_ppt = Presentation(self.template_path)
+        clear_presentation(output_ppt)
+        extra_namespace = {}
+        slides_managed = []
+        for slide_no, slide in enumerate(template_ppt.slides):
+            if slide_no in slides_managed:
+                # this slide has already been looped over
+                continue
+            slide_used = False
+            for loop_group in loop_groups:
+                if slide_no == loop_group["start"]:
+                    slide_used = True
+                    for variable_value in loop_group["iterable"]:
+                        for loop_slide_no in range(
+                            loop_group["start"], loop_group["end"] + 1
+                        ):
+                            if loop_slide_no not in slides_managed:
+                                slides_managed.append(loop_slide_no)
+                            # get the slide from the template
+                            current_slide = template_ppt.slides[loop_slide_no]
+                            # add a copy of this slide to output_slides
+                            new_slide = copy_slide(template_ppt, output_ppt, current_slide)
+                            extra_namespace[output_ppt.slides.index(new_slide)] = {
+                                loop_group["variable"]: variable_value
+                            }
+            if not slide_used:
+                # this slide is not part of a loop group
+                new_slide = copy_slide(template_ppt, output_ppt, slide)
+        for slide_no, slide in enumerate(output_ppt.slides):
+            self.namespace.update(extra_namespace.get(slide_no, {}))
+            if slide.has_notes_slide and slide.notes_slide.notes_text_frame:
                 python_code = re.search(
                     r"```python([\s\S]*)```",
                     fix_quotes(slide.notes_slide.notes_text_frame.text),
@@ -171,4 +213,4 @@ class PPTXRenderer:
                     exec(python_code.group(1), self.namespace)
             #send list of shapes to handle_shapes function
             handle_shapes(list(slide.shapes))
-        outppt.save(output_path)
+        output_ppt.save(output_path)
