79d655e02f2039d88a1cd5a4b870c7850470ec9b
Najeem Muhammed <najeem@gmail.com>
2024-12-07 22:05:15 +0100

    Added tests for loop_groups

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,7 +12,7 @@ def mymethod(abc):
 
 
 def getimage():
-    return "docs/_src/_static/gkn_only_small.png"
+    return "docs/_src/_static/is_it_worth_the_time.png"
 
 
 mytable = [["a", "b", "c", "d", "e"]] * 10
@@ -60,3 +60,28 @@ def test_multi_variable_textbox():
     for shape in slide.shapes:
         if hasattr(shape, "text") and shape.text.startswith("The value of X is"):
             assert shape.text.strip() == "The value of X is 1 and the value of Y is 10."
+
+def test_loop_group():
+    template = Presentation("template.pptx")
+    slide = template.slides[0]
+    title = slide.shapes.title
+    title.text = "Loop Group Test - {{{i}}}"
+    template.save("template-loop-group.pptx")
+    p = PPTXRenderer("template-loop-group.pptx")
+    someval = "hello"
+    def mymethod(abc):
+        return f"{abc} " * 5
+    p.render(
+        "output.pptx",
+        {
+            "someval": someval,
+            "mymethod": mymethod,
+            "myimage": getimage(),
+            "mytable": mytable,
+        },
+        loop_groups=[{"start": 0, "end": 0, "variable": "i", "iterable": [1, 2, 3]}],
+    )
+    prs = Presentation("output.pptx")
+    assert len(prs.slides) == 3
+    slide = prs.slides[0]
+    assert slide.shapes.title.text == "Loop Group Test - 1"
